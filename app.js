c386fb7e185a86498f419e2b2b5b14baa84ef657
Piyushrathoree <meonpath008@gmail.com>
2025-07-16 16:59:16 +0530

    Update CORS configuration and add missing scripts in package.json

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: "http://localhost:5173",
-        methods: ["GET", "POST", "PUT", "DELETE"],
+        origin: {"http://localhost:5173","https://doctor-appointment-system-client-iota.vercel.app/login"}
+        ,methods: ["GET", "POST", "PUT", "DELETE"],
     })
 );
 import appointmentRoutes from "./routes/appointment.routes.js";
